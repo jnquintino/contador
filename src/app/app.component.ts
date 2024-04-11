0ffc8cf9e3d628ed9015ebc12db7777fe5f2e089
Joao Quintino <mei@syx.com.br>
2024-04-11 19:37:41 -0300

    Codigo inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,6 +35,9 @@ export class AppComponent implements OnInit {
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-    return `${days} dias ${hours} horas ${minutes} minutos ${seconds} segundos`;
+    return days + (days > 1 ? ' dias ' : ' dia ') +
+      hours + (hours > 1 ? ' horas ' : ' hora ') +
+      minutes + (minutes > 1 ? ' minutos ' : ' minuto ') +
+      seconds + (seconds > 1 ? ' segundos' : ' segundo');
   }
 }
