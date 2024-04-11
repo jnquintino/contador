5b86453d7b1a821e148e19827d83e5bac3d177c4
Joao Quintino <mei@syx.com.br>
2024-04-11 19:28:16 -0300

    Codigo inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'contador';
+export class AppComponent implements OnInit {
+  title = 'Contador [2026]';
+  countdownInterval: any;
+  finish = new Date(2026, 11, 31, 23, 59, 59, 999);
+  show = '';
+
+  ngOnInit() {
+    this.countdownInterval = setInterval(() => {
+      const now = new Date();
+      if (now > this.finish) {
+        this.show = 'Contador terminado';
+        clearInterval(this.countdownInterval);
+      }
+      this.show = this.contador(now);
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.countdownInterval);
+  }
+
+  private contador(now: Date) {
+    const timeLeft = this.finish.getTime() - now.getTime();
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    return `${days} dias ${hours} horas ${minutes} minutos ${seconds} segundos`;
+  }
 }
